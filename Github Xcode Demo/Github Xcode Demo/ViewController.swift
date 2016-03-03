109801aa26f102257d6f3d07313e1441ebea591a
Paul Solt <PaulSolt@gmail.com>
2016-03-03 15:42:20 -0500

    Revert "Added a comment about a new change"
    This reverts commit 06dd21ea07702266c3923527a899e7a3f71b3588.

--- Github Xcode Demo/Github Xcode Demo/ViewController.swift
+++ Github Xcode Demo/Github Xcode Demo/ViewController.swift
@@ -14,8 +14,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         print("Hello Paul!")
-        
-        // 2nd change in our commit (hey there)
     
         doSomething()
     }
