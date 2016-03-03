06dd21ea07702266c3923527a899e7a3f71b3588
Paul Solt <PaulSolt@gmail.com>
2016-03-03 15:40:48 -0500

    Added a comment about a new change

--- Github Xcode Demo/Github Xcode Demo/ViewController.swift
+++ Github Xcode Demo/Github Xcode Demo/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         print("Hello Paul!")
+        
+        // 2nd change in our commit (hey there)
     
         doSomething()
     }
