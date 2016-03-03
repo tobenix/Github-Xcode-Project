a831ae32c37aa50026213ab26d2edb4b59e5280a
Paul Solt <PaulSolt@gmail.com>
2016-03-03 15:39:27 -0500

    Updated print statement with name

--- Github Xcode Demo/Github Xcode Demo/ViewController.swift
+++ Github Xcode Demo/Github Xcode Demo/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("Hello!")
+        print("Hello Paul!")
     
         doSomething()
     }
