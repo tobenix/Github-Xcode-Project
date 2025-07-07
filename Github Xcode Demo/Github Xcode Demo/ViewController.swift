84ec43678f9e7568e1f0c342416c91ad7b8f4f57
tobenix2025@gmail.com <tobenix2025@gmail.com>
2025-07-07 13:36:47 +0900

    refix

--- Github Xcode Demo/Github Xcode Demo/ViewController.swift
+++ Github Xcode Demo/Github Xcode Demo/ViewController.swift
@@ -14,11 +14,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         print("Hello!")
-
+        
+        // Added 1st statement on Branch Update-the-UI
+    
         doSomething()
-
-
+    }
+    
     func doSomething() {
         print("do some algorithm and computation")
     }
+
 }
